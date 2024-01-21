c3120a029b740ea60cce48a500f4a01a42fa72d9
Mahavishnu <mahavishnu721@gmail.com>
2024-01-21 09:53:22 +0530

    text file create

--- userapp1.c
+++ userapp1.c
@@ -7,8 +7,10 @@
 
 int main()
 {
+	FILE* fptr;
 	char c='h';
 	int dev=open("/dev/kerdriver",O_RDWR);
+	printf("current process id %d\n",getpid());
 	if(dev<0)
 	{
 		printf("driver cannot open\n");
@@ -26,6 +28,13 @@ int main()
 	//ioctl(dev,STRUCT_WR,&s);
 	printf("data send to kernel \n");
 	char buf[200];*/
+	fptr=fopen("spy.txt","a");
+	if(fptr==NULL)
+	{
+		printf("spy not open \n");
+		return 0;
+	}
+	
 	ioctl(dev,DATA_RD,&c);
 	printf("data from kernel is : %c\n",c);
 	
@@ -35,6 +44,9 @@ int main()
 	printf("%s\n",s.data);
 	*/
 	
+	putc(c,fptr);
+	
+	fclose(fptr);
 	close(dev);
 	printf("driver is close\n");
 	return 0;
